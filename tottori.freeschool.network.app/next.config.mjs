e0e8ab4cdf79dde4e9ae339048bad221ea5cf158
tohma.A <tohma.a.hello.jp@gmail.com>
2024-10-16 12:25:41 +0900

    douo

--- tottori.freeschool.network.app/next.config.mjs
+++ tottori.freeschool.network.app/next.config.mjs
@@ -1,6 +1,10 @@
 // @type {import('next').NextConfig} 
 const nextConfig = {
-    output: `export`
+    output: `export`,
+    experimental: {
+        appDir: true, // 必要に応じて実験的機能を有効化
+      },
+      reactStrictMode: true, // リアクトの厳密モードを有効にする
 }
 
 export default nextConfig;
