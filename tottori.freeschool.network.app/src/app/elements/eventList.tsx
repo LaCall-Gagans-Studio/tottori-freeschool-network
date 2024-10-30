4c12adcd8cb182ed264e27935064c445a8eef59e
tohma.A <tohma.a.hello.jp@gmail.com>
2024-10-30 15:01:41 +0900

    un

--- tottori.freeschool.network.app/src/app/elements/eventList.tsx
+++ tottori.freeschool.network.app/src/app/elements/eventList.tsx
@@ -29,8 +29,8 @@ const EventList: React.FC<EventListProps> = ({ events }) => {
     };
 
     return (
-        <div className="p-4 pt-12 h-[calc(100svh*11/12)] lg:h-full w-full mx-auto overflow-y-scroll bg-ws-transparent lg:bg-transparent z-20">
-            <div className="w-full lg:max-w-[800px] lg:w-4/6 h-auto mx-auto grid grid-cols-1 gap-4 lg:gap-8 items-center relative">
+        <div className="p-4 pt-12 h-[calc(100svh*11/12)] lg:h-full w-full mx-auto overflow-y-scroll bg-[#f8fdee] lg:bg-transparent z-20">
+            <div className="w-full lg:max-w-[800px] lg:w-4/6 h-auto mx-auto grid grid-cols-1 gap-6 lg:gap-8 items-center relative">
                 
                 <div className="w-auto text-nowrap text-sm lg:text-base h-auto text-ws-black absolute top-6 text-center left-1/2 -translate-x-1/2 z-10">
                     <p>おっと、何もないようです。</p>
