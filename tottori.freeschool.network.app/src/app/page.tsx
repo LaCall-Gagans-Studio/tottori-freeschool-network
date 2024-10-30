feaac587a63550ac6785ed2a33a9325c558670d7
tohma.A <tohma.a.hello.jp@gmail.com>
2024-10-30 14:09:35 +0900

    dsa

--- tottori.freeschool.network.app/src/app/page.tsx
+++ tottori.freeschool.network.app/src/app/page.tsx
@@ -64,7 +64,7 @@ const Home = () => {
 
   return (
     <div className="w-screen lg:bg-[#f8fdee] font-zenGothic relative h-screen bg-yellow-500">
-      <div className="flex flex-col lg:flex-row w-full lg:h-screen bg-red-300 z-0">
+      <div className="flex flex-col lg:flex-row w-full lg:h-screen h-auto bg-red-300 z-0">
         <div className="h-[calc(100svh*1/12)] lg:basis-auto lg:h-full w-full lg:w-1/6 order-2 lg:order-1">
           <Menu toggleView={toggleView} isMapView={isMapView} toggleTag={toggleTag} selectedTags={selectedTags} />
         </div>
