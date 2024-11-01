c8f7de6bdea97a6084909650772c7e45fa531d73
tohma.A <tohma.a.hello.jp@gmail.com>
2024-11-01 15:32:22 +0900

    meta changed

--- tottori.freeschool.network.app/src/app/layout.tsx
+++ tottori.freeschool.network.app/src/app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
     <html lang="ja">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="google-site-verification" content="F6aPhFzrcmNE0_vgJEbhk0u03n78Sgpt0j1noL26eYI" />
+        <meta name="google-site-verification" content="EzH28jpTZShqeDUipsnA9Hjd5yTcKMGEPQQ4hkPLjFU" />
         <link rel="canonical" href="https://tottori-yawaraka-infos.org" />
         <script type="application/ld+json">
           {JSON.stringify({
