699a04c10912673d631a0acef8f2fd1a299351dd
tohma.A <tohma.a.hello.jp@gmail.com>
2024-11-10 13:56:55 +0900

    aaaa

--- tottori.freeschool.network.app/src/app/page.tsx
+++ tottori.freeschool.network.app/src/app/page.tsx
@@ -26,7 +26,7 @@ const App = () => {
                 </Router>
             ) : (
                 <div className='inset-0 bg-ws-gray cursor-default w-screen h-screen flex flex-col items-center justify-center gap-6 mx-auto'>
-                    <img src='/common/logo.webp' className='w-96 h-auto' />
+                    <img src='common/logo.webp' alt='ロゴ' className='w-96 h-auto' />
                     <p className='text-black'>読み込み中...</p>
                     <h1 className='opacity-0'>鳥取県内のフリースクールを詳しくまとめ、一覧にしています</h1>
                 </div>
