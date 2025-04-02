49dfe8b8b083dfa591915c146d0d305e78ab9e15
tohma.A <tohma.a.hello.jp@gmail.com>
2025-04-02 18:51:57 +0900

    yu

--- tottori.freeschool.network.app/next.config.js
+++ tottori.freeschool.network.app/next.config.js
@@ -3,10 +3,23 @@ const nextConfig = {
   env: {
     GITHUB_TOKEN: process.env.GITHUB_TOKEN,
   },
+  experimental: {
+    serverComponentsExternalPackages: [
+      // Cloudflare側に送らず外部参照させる依存（重そうなやつ）
+      "react-markdown",
+      "gray-matter",
+      "js-yaml",
+      "firebase",
+      "@octokit/rest",
+      "leaflet",
+      "react-leaflet",
+    ],
+  },
   webpack: (config) => {
     if (config.optimization?.splitChunks) {
-      config.optimization.splitChunks.maxSize = 2000000; // 2MB 分割
-      config.optimization.splitChunks.minSize = 100000;  // 100KB 開始
+      config.optimization.splitChunks.maxSize = 2 * 1024 * 1024; // 2MB
+      config.optimization.splitChunks.minSize = 100 * 1024; // 100KB
+
       config.optimization.splitChunks.cacheGroups = {
         defaultVendors: {
           test: /[\\/]node_modules[\\/]/,
