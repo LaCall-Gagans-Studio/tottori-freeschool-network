40d70addb35b3b5dfa25aca00cf04d63df5059a5
tohma.A <tohma.a.hello.jp@gmail.com>
2025-04-02 18:15:25 +0900

    das

--- tottori.freeschool.network.app/next.config.js
+++ tottori.freeschool.network.app/next.config.js
@@ -4,11 +4,9 @@ const nextConfig = {
         GITHUB_TOKEN: process.env.GITHUB_TOKEN,
     },
     webpack: (config) => {
-        config.optimization.splitChunks.maxSize = 20000000; // 20MB に分割
+        config.optimization.splitChunks.maxSize = 20000000;
         return config;
     },
-}
+};
 
-export default nextConfig
-
-// next.config.js
+module.exports = nextConfig;
