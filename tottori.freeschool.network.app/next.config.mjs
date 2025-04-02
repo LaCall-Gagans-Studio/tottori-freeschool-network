6f97692a054774dafe72c9732ca9ccf526016a49
tohma.A <tohma.a.hello.jp@gmail.com>
2025-04-02 13:29:25 +0900

    a

--- tottori.freeschool.network.app/next.config.mjs
+++ tottori.freeschool.network.app/next.config.mjs
@@ -1,6 +1,9 @@
-// @type {import('next').NextConfig} 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: `export`
-}
-
-export default nextConfig;
+    env: {
+        GITHUB_TOKEN: process.env.GITHUB_TOKEN,
+      },
+  }
+  
+  export default nextConfig
+  
