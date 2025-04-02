2fabeb3f1e32fe52072936e71eb7693f16c81e6f
tohma.A <tohma.a.hello.jp@gmail.com>
2025-04-02 18:32:45 +0900

    ayy

--- tottori.freeschool.network.app/next.config.js
+++ tottori.freeschool.network.app/next.config.js
@@ -1,14 +1,18 @@
-import type { Configuration } from 'webpack';
-
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer({
-  webpack: (config: Configuration) => {
+/** @type {import('next').NextConfig} */
+const nextConfig = withBundleAnalyzer({
+  env: {
+    GITHUB_TOKEN: process.env.GITHUB_TOKEN,
+  },
+  webpack: (config) => {
     if (config.optimization?.splitChunks) {
       config.optimization.splitChunks.maxSize = 20000000;
     }
     return config;
   },
 });
+
+module.exports = nextConfig;
