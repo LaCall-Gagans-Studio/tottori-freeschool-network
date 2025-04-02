c6b6555ca1dca7fa462d372805ae26f43428b865
tohma.A <tohma.a.hello.jp@gmail.com>
2025-04-02 18:26:14 +0900

    ay yo

--- tottori.freeschool.network.app/next.config.js
+++ tottori.freeschool.network.app/next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        GITHUB_TOKEN: process.env.GITHUB_TOKEN,
+      GITHUB_TOKEN: process.env.GITHUB_TOKEN,
     },
-    webpack: (/** @type {any} */ config) => {
-        config.optimization.splitChunks.maxSize = 20000000;
-        return config;
+    webpack: (/** @type {import('webpack').Configuration} */ config) => {
+      config.optimization.splitChunks.maxSize = 20000000;
+      return config;
     },
-};
-
-module.exports = nextConfig;
+  };
+  
+  module.exports = nextConfig;
+  
